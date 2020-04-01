2cfa57dfd8cbad23dcdc1b2ab11e5cacd613618f
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2020-04-02 03:44:53 +0900

    Foe heroku -7

--- main.py
+++ main.py
@@ -28,27 +28,27 @@ handler = WebhookHandler(CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+   # get X-Line-Signature header value
+   signature = request.headers['X-Line-Signature']
+   # get request body as text
+   body = request.get_data(as_text=True)
+   app.logger.info("Request body: " + body)
+   # handle webhook body
+   try:
+       handler.handle(body, signature)
+   except InvalidSignatureError:
+       abort(400)
+   return 'OK'
 
-    body = request.get_data(as_text=True)
-    app.logger.info("Request Body: " + body)
-
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_tpken,
-        TextSendMessage(text="event.message.text"))
+	line_bot_api.reply_message(
+       event.reply_token,
+       TextSendMessage(text=event.message.text)
+    )
+
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
-
-
+   port = int(os.getenv("PORT"))
+   app.run(host="0.0.0.0", port=port)
