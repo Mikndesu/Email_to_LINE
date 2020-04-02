d6f2c829937cc8b84349cf927be4cb9d901f6096
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2020-04-02 16:11:12 +0900

    Change Response Message if i send "Update"

--- main.py
+++ main.py
@@ -46,9 +46,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if (event.message.text) == 'checkUpdate':
+        message = "Update"
+    else:
+        message = event.message.text
+
     line_bot_api.reply_message(
        event.reply_token,
-       TextSendMessage(text=event.message.text)
+       TextSendMessage(text=message)
     )
 
 
