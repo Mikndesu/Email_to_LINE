d0b463063063db74c9b834a20c68e64acb620276
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2020-04-05 14:54:46 +0900

    fix Did't get gmail id

--- GetGmail.py
+++ GetGmail.py
@@ -26,26 +26,25 @@ class GMail:
             'Authorization' : 'Bearer ' + self.access_token,
         }
         res = requests.get('https://www.googleapis.com/gmail/v1/users/me/messages/', headers=headers)
-        print(res)
-        messageIdsOrError = res.json()
-        if "error" in messageIdsOrError == True:
-            # generate new access_token if previous value is unavailable
-            data = {
-                'client_id' : self.client_id,
-                'client_secret' : self.client_secret,
-                'refresh_token' : self.refresh_token,
-                'grant_type' : 'refresh_token'
+        if res.status_code!=200:
+            self.generateNewAccessToken()
+            header = {
+                'Authorization': 'Bearer '+  self.access_token,
             }
-            response = requests.post('https://accounts.google.com/o/oauth2/token', data=data)
-            print(response)
-            os.environ["ACCESS_TOKEN"] = response["access_token"]
-            headers = {
-                'Authorization': 'Bearer ' + self.access_token,
-            }
-            resp = requests.get('https://www.googleapis.com/gmail/v1/users/me/messages/', headers=headers)
-            print(resp)
-            messageIds = resp.json()
-            return messageIds
+            resp = requests.get('https://www.googleapis.com/gmail/v1/users/me/messages/', headers=header)
+            return resp.json()
         else:
-            return messageIdsOrError
+            return res.json()
 
+    def generateNewAccessToken(self):
+        # gen new access_token if previous one is available
+        # replace env with it
+        data = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'refresh_token': self.refresh_token,
+            'grant_type': 'refresh_token'
+        }
+        response = requests.post('https://accounts.google.com/o/oauth2/token', data=data).json()
+        os.environ["ACCESS_TOKEN"] = response["access_token"]
+        self.access_token = response["access_token"]
